37c0b28f6cabee29494cfedb94944550d7781f1b
stylestech1 <stylestech1@gmail.com>
2025-09-30 07:24:55 -0700

    first push 2

---
+++ src/components/ui/Btns.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Btns = () => {
+  return (
+    <div>Btns</div>
+  )
+}
+
+export default Btns
---
+++ src/components/ui/Taps.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Taps = () => {
+  return (
+    <div>Taps</div>
+  )
+}
+
+export default Taps
