36d7466c6dfac132e6d3e400859589f7dd1f90bc
stylestech1 <stylestech1@gmail.com>
2025-11-04 14:59:47 +0200

    loading page

--- src/components/ui/Loading.tsx
+++ src/components/ui/Loading.tsx
@@ -1,8 +1,9 @@
 "use client";
 export default function Loading() {
   return (
-    <div className="flex items-center justify-center h-full w-full py-10">
+    <div className="flex flex-col items-center justify-center h-[100vh] w-full py-10">
       <div className="w-10 h-10 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+      <h2 className="my-5 text-xl">Loading...</h2>
     </div>
   );
 }
