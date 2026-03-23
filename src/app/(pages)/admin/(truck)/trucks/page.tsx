9697accca2c559ee50aa6d7b47d2143112b7ce97
Rana Elhabashy <ranahabashy85@gmail.com>
2026-03-23 17:48:26 +0200

    make totalmileage without decimal point

--- src/app/(pages)/admin/(truck)/trucks/page.tsx
+++ src/app/(pages)/admin/(truck)/trucks/page.tsx
@@ -511,8 +511,10 @@ const TrucksPage: React.FC = () => {
         </td>
 
         <td className="p-4 text-center">{t.fuelPerMile ?? "N/A"}</td>
-        <td className="p-4 text-center">{t.totalMileage ?? "N/A"}</td>
-
+        <td className="p-4 text-center">
+          {t.totalMileage != null ? Math.trunc(Number(t.totalMileage)) : "N/A"}
+        </td>
+        
         <td className="p-4 text-center">
           <StatusChip status={t.status as any} />
         </td>
