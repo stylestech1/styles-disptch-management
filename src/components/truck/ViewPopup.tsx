d64a047e63759a57923283da93af12d9d2610414
Rana Elhabashy <ranahabashy85@gmail.com>
2026-08-19 23:05:11 +0300

    update viewpopup in truck

--- src/components/truck/ViewPopup.tsx
+++ src/components/truck/ViewPopup.tsx
@@ -101,7 +101,8 @@ export function ViewTruckDialog({
     selectedTruck: any;
     theme: any;
 }) {
-    const truckId = selectedTruck?.truckId ?? selectedTruck?.id ?? "—";
+    
+    const truckId = selectedTruck?.truckNumber ?? "—";
     const status = selectedTruck?.status ?? "N/A";
 
     const driverName =
